30ea62b05b02eade7f62856e1e7c0b609dd8aaff
se7enack <steburke71@hotmail.com>
2024-02-03 13:14:10 -0500

    Healthcare Cost Transparency Tool

---
+++ README.md
@@ -0,0 +1,46 @@
+# Healthcare Cost Transparency Tool 
+Tool for dynamic cost lookups for Medical Facilities (for MacOS)
+
+## Open the Terminal.app via Finder
+<img width="571" alt="finder1" src="https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/assets/18600808/04aa8170-a83f-49ee-8e9a-c647bd1120d7">
+
+
+## Install The Healthcare Cost Transparency Tool via Terminal.app
+```
+git clone https://github.com/se7enack/Healthcare-Cost-Transparency-Tool.git
+cd Healthcare-Cost-Transparency-Tool/mac-app/
+unzip Healthcare-Cost-Transparency-Tool.zip
+rm -fr ~/Applications/Healthcare-Cost-Transparency-Tool.app
+mv Healthcare-Cost-Transparency-Tool.app ~/Applications/.
+```
+## Search for the app in Finder and click it
+![finder2](https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/assets/18600808/76b748e7-fac3-4fd6-b8d8-e3675afdeddf)
+
+
+## Right click the Icon and pin it to the dock
+<img width="276" alt="dock" src="https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/assets/18600808/275f4ac1-766b-4f88-a512-f60fb345dc1d">
+
+
+## Using the app
+
+### Demo
+<video src="https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/assets/18600808/3ece2f00-2fb0-47f7-8b6b-c564d6465411" controls="controls" style="max-width: 730px;"> </video>
+
+<i>*The data used to for this demo is in no way factual. It exists solely to demonstrate the app. The amounts have all been scrambled.</i>
+
+This Price-Transparency tool is licensed under the "MIT License" and it can be viewed here:
+
+https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/blob/main/LICENSE
+
+The source code used to build the app can be viewed here:
+
+https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/blob/main/main.py
+
+
+***
+If you are from a medical orginzation looking for a tool like this please reach out by email. 
+
+Please include a web-link to your public facing pricing files and we'll look into porting this to work for you.
+
+---------------------------------> <img width="198" alt="email" src="https://github.com/se7enack/Healthcare-Cost-Transparency-Tool/assets/18600808/041925f0-6e6f-4a71-90cd-28c29865574c">
+ <---------------------------------
---
+++ main.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+
+import json
+import csv
+import time
+import os
+from tkinter import *
+import easygui
+import requests
+import shutil
+
+title = "Healthcare Cost Transparency Tool"
+
+facilities = []
+sites = {}
+allurls = {}
+
+
+class Hospital:
+    def __init__(self, location, url):
+        global facilities
+        self.location = location
+        self.url = url
+        facilities.append(location)
+        sites[location] = self
+        allurls[location] = url
+
+
+################################################################################################################
+# Add new hospitals here. The url should point to your .csv pricelist (open the url below for example formating)
+h01 = Hospital("Local General Hospital", "https://github.com/se7enack/randomfiles/raw/main/hospital1.csv") 
+h02 = Hospital("Big City Hospital", "https://github.com/se7enack/randomfiles/raw/main/hospital1.csv")
+h03 = Hospital("ACME Medical Center", "https://github.com/se7enack/randomfiles/raw/main/hospital1.csv")
+################################################################################################################
+
+
+def vars(x):
+    global csv_from_hospital_path
+    global json_file_path
+    global csv_file_path
+    csv_from_hospital_path = f"{x}-original.csv"
+    csv_file_path = f"{x}-fixed.csv"
+    json_file_path = f"{x}.json"
+
+
+def pullfile(url):
+    # Pull file down from hosptial website
+    there = os.path.exists(json_file_path)
+    if there:
+        old = time.time() - os.path.getmtime(json_file_path) > (3 * 30 * 24 * 60 * 60)
+    if there == False or old:
+        print(f"{csv_from_hospital_path} is missing or older than 24hrs, downloading a fresh copy")
+        try:
+            r = requests.get(url, verify=False,stream=True)
+            r.raw.decode_content = True
+            with open(csv_from_hospital_path, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)            
+            found = 0
+            headerrow = "Code"
+            with open(csv_from_hospital_path, "r") as input:
+                with open(csv_file_path, "w") as output: 
+                    for line in input:
+                        if line.startswith(headerrow):
+                            found = 1
+                        if found == 0:
+                            pass
+                        else:
+                            output.write(line)
+            remove_file(csv_from_hospital_path)
+            load_json_from_csv_file_with_headers(csv_file_path, json_file_path)
+        except:
+            if there:
+                print("unable to download a fresh copy at this time, using local copy")
+                jsonify()
+            else:
+                exit(1)
+    else:
+        print(f"{csv_from_hospital_path} is newer than 24hrs, skipping download")
+        jsonify()
+
+
+def start():
+    global data
+    global records
+    global facility
+    message = "Choose a facility"
+    facility = easygui.buttonbox(message, title, facilities)
+    happy = bool(facility)
+    if happy == True:
+        vars(sites[facility])
+        url = allurls[facility]
+        pullfile(url)
+    else:
+        exit(0)      
+  
+        
+def load_json_from_csv_file_with_headers(csv_file_path, json_file_path):
+    with open(csv_file_path, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        headers = next(reader)   
+        data = []
+        for row in reader:
+            data.append(dict(zip(headers, row)))
+    with open(json_file_path, "w") as json_file:
+        json.dump(data, json_file)
+    remove_file(csv_file_path)
+    jsonify()      
+    
+    
+def jsonify():
+    global records
+    global data
+    f = open(json_file_path)
+    data = json.load(f)
+    f.close()
+    records = len(data)  
+    asktype()              
+
+ 
+def asktype():
+    message = "What are you looking to do?"
+    choices = ["Search by CPT", "Search by Description", "Exit"]
+    output = easygui.buttonbox(message, title, choices)
+    try:
+        if output == "Search by CPT":
+            options = menulevel("Code")
+            cpt = easygui.choicebox("Pick the item you want cost for:", "CPT Lookup", options)
+            happy = bool(cpt)
+            if happy == True:
+                pass
+            else:
+                exit(0)
+            k = level2("Code", cpt)
+        elif output == "Search by Description":
+            options = menulevel("Description")
+            description = easygui.choicebox("Pick the item you want cost for", "Description Lookup", options)
+            happy = bool(description)
+            if happy == True:
+                pass
+            else:
+                exit(0)            
+            k = level2("Description", description)
+        else:
+            exit(0)
+    except:
+        exit(0)
+    # k is a list of the column headers  
+    k = list(k)
+    # Remove the first three columns headers ("Cost", "Description", and "Type") so they aren't in the next menu
+    # In some cases 'Package/Line_Level' is there, if so it is also removed
+    if 'Package/Line_Level' in k:
+        del k[0:4:1]
+    else:
+        del k[0:3:1]
+    message = "Which charge would you like to see?"
+    choice = easygui.choicebox(message, title, k)
+    happy = bool(choice)
+    if happy == True:
+        pass
+    else:
+        exit(0)       
+    try:
+        # If price is a float make sure it contains 2 decimal points and add "$" prefix
+        flt = float(data[record][choice])
+        cash = f"{flt:.2f}"
+        money = str(f"\n\n\n\n{data[record]['Description']}\n\n\n\nCPT Code: {data[record]['Code']}\n\n\n\n{choice}: ${cash}")
+    except:
+        money = data[record][choice]     
+    choices = ["Do another", "Quit"]
+    p = easygui.buttonbox(money, facility, choices)
+    if p == "Do another":
+        start()  
+
+
+def remove_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        
+        
+def level2(k, v):
+    global record
+    for record in range(records):
+        if data[record][k] == v:
+            return data[record].keys()
+
+
+def menulevel(c):
+    # Return a list of items in the c column
+    values = []
+    for record in range(records):
+        x = data[record][c]
+        values.append(x) 
+    return values
+
+
+start()
